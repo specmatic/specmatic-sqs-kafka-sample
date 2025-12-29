e2413726ca1de1eb7f8bd00ae04f175d47d33c58
Specmatic Renovate <github-service-account@specmatic.io>
2025-12-29 05:15:48 +0000

    chore(deps): update dependency org.slf4j:slf4j-api to v2.0.17

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     implementation("aws.smithy.kotlin:http-client-engine-crt:1.0.0")
     implementation("org.apache.kafka:kafka-clients:3.6.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("org.slf4j:slf4j-api:2.0.9")
+    implementation("org.slf4j:slf4j-api:2.0.17")
     implementation("ch.qos.logback:logback-classic:1.4.14")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
