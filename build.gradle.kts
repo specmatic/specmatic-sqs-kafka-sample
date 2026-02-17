87fbd2fdaf24ea4e4d546d21f6a713714d5d00f0
Specmatic Renovate <github-service-account@specmatic.io>
2026-02-17 00:28:53 +0000

    chore(deps): update dependency aws.sdk.kotlin:sqs to v1.6.18

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 }
 
 dependencies {
-    implementation("aws.sdk.kotlin:sqs:1.6.17")
+    implementation("aws.sdk.kotlin:sqs:1.6.18")
     implementation("aws.smithy.kotlin:http-client-engine-crt:1.6.2")
     implementation("org.apache.kafka:kafka-clients:3.9.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
