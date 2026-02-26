eac73f4bec58dab0a31bdbb56922c6f76708a97a
Specmatic Renovate <github-service-account@specmatic.io>
2026-02-26 08:22:59 +0000

    chore(deps): update dependency org.apache.kafka:kafka-clients to v4

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 dependencies {
     implementation("aws.sdk.kotlin:sqs:1.6.25")
     implementation("aws.smithy.kotlin:http-client-engine-crt:1.6.5")
-    implementation("org.apache.kafka:kafka-clients:3.9.2")
+    implementation("org.apache.kafka:kafka-clients:4.2.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     implementation("org.slf4j:slf4j-api:2.0.17")
     implementation("ch.qos.logback:logback-classic:1.5.32")
