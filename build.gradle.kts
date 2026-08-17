f4fea191ffc94791457b7e6e59c9a2718e6b38ea
Specmatic Renovate <github-service-account@specmatic.io>
2026-08-17 00:26:28 +0000

    chore(deps): update jackson monorepo to v2.22.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     implementation("org.slf4j:slf4j-api:2.0.17")
     implementation("ch.qos.logback:logback-classic:1.5.32")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.21.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.22.2")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.21.2")
 
     testImplementation(kotlin("test"))
