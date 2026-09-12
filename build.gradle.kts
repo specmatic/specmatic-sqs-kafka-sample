6af4e509a705a2f152567818d353e28efc449062
Specmatic Renovate <github-service-account@specmatic.io>
2026-09-12 00:38:01 +0000

    chore(deps): update dependency aws.smithy.kotlin:http-client-engine-crt to v1.7.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 
 dependencies {
     implementation("aws.sdk.kotlin:sqs:1.8.47")
-    implementation("aws.smithy.kotlin:http-client-engine-crt:1.7.9")
+    implementation("aws.smithy.kotlin:http-client-engine-crt:1.7.10")
     implementation("org.apache.kafka:kafka-clients:3.9.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
     implementation("org.slf4j:slf4j-api:2.0.19")
